f8a3c6ac7f088fe6391783af0f0a5184d2bf02ab
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-26 22:24:43 +0300

    create an instance of MyPanel

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -3,7 +3,12 @@ import java.awt.*;
 
 public class MyFrame extends JFrame {
 
+    MyPanel panel;
+
     MyFrame(){
+
+        panel = new MyPanel();
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
         this.setLocationRelativeTo(null);
