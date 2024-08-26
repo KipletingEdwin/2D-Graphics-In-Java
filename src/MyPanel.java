67ef506242413ceaa3ff28fb5e8cbd63d896c0a9
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-26 22:47:16 +0300

    setStroke to BasicStroke of 5

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -5,12 +5,13 @@ public class MyPanel extends JPanel {
 
     MyPanel(){
         this.setPreferredSize(new Dimension(500,500));
-
     }
 
     public  void  paint(Graphics g){
+
         Graphics2D g2d = (Graphics2D) g;
 
+        g2d.setStroke(new BasicStroke(5));
         g2d.drawLine(0,0,500,500);
     }
 }
