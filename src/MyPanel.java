be02bf60f230edd6f4fcc0d19d856371b37b8875
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-26 22:54:02 +0300

    fillRect

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -15,6 +15,11 @@ public class MyPanel extends JPanel {
         g2d.setPaint(Color.blue);
         g2d.setStroke(new BasicStroke(5));
         //g2d.drawLine(0,0,500,500);
+
+
         g2d.drawRect(0,0,100,200);
+        //g2d.setPaint(Color.pink);
+        g2d.fillRect(0,0,100,200);
+
     }
 }
